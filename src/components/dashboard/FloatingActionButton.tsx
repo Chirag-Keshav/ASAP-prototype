b2e410ab8e292efbb1654bc6ad574b1f2eccb113
Chirag K <chiragkeshav.ck@gmail.com>
2025-09-07 04:37:39 +0000

    make the add request button in the customer dashboard functional please.

--- src/components/dashboard/FloatingActionButton.tsx
+++ src/components/dashboard/FloatingActionButton.tsx
@@ -2,14 +2,20 @@
 
 import { Plus } from "lucide-react";
 import { Button } from "../ui/button";
+import React from "react";
 
-export const FloatingActionButton = () => {
-    return (
-        <Button
-            className="fixed bottom-8 right-8 h-16 w-16 rounded-full shadow-lg"
-            aria-label="New Delivery Request"
-        >
-            <Plus className="h-8 w-8" />
-        </Button>
-    )
-}
+export const FloatingActionButton = React.forwardRef<HTMLButtonElement, React.ButtonHTMLAttributes<HTMLButtonElement>>(
+    (props, ref) => {
+        return (
+            <Button
+                ref={ref}
+                className="fixed bottom-8 right-8 h-16 w-16 rounded-full shadow-lg"
+                aria-label="New Delivery Request"
+                {...props}
+            >
+                <Plus className="h-8 w-8" />
+            </Button>
+        )
+    }
+);
+FloatingActionButton.displayName = 'FloatingActionButton';
